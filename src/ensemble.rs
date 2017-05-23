09b19c1cfb7ef9afa3b59e4ed7c37b23b5018b8d
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-05-23 17:41:10 +0900

    Add comment

--- src/ensemble.rs
+++ src/ensemble.rs
@@ -5,6 +5,7 @@ use ndarray_rand::RandomExt;
 
 use super::types::*;
 
+/// Ensemble is saved as two-dimensional array
 #[derive(Debug, Clone)]
 pub struct EnsembleBase<S: DataClone<Elem = R>>(ArrayBase<S, Ix2>);
 pub type Ensemble = EnsembleBase<OwnedRepr<R>>;
@@ -16,12 +17,13 @@ impl<S: DataClone<Elem = R>> From<ArrayBase<S, Ix2>> for EnsembleBase<S> {
 }
 
 impl<S: DataClone<Elem = R>> EnsembleBase<S> {
+    /// Generate ensemble from the center vector and noise intensity
     pub fn generate<S1>(center: &ArrayBase<S1, Ix1>, size: usize, noise: R) -> Ensemble
         where S1: Data<Elem = R>
     {
         let n = center.len();
         let dist = Normal::new(0.0, noise);
-        let dx = Array2::random((n, size), dist);
+        let dx = Array::random((n, size), dist);
         (dx + center).into()
     }
 }
