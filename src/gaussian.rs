97cb0228a367757d4be769c12e8f6e030e48659a
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-05-31 16:13:00 +0900

    Rename PGaussian

--- src/gaussian.rs
+++ src/gaussian.rs
@@ -1,7 +1,7 @@
-
 use super::types::*;
 use ndarray::*;
 
+/// Gaussian distribution
 #[derive(Debug, Clone)]
 pub struct GaussianBase<S1, S2>
 where
@@ -14,8 +14,9 @@ where
 
 pub type Gaussian = GaussianBase<OwnedRepr<R>, OwnedRepr<R>>;
 
+/// `exp(|y-Hx|^2)` type Gaussian
 #[derive(Debug, Clone)]
-pub struct ProjectedGaussian<S1, S2, S3>
+pub struct PGaussianBase<S1, S2, S3>
 where
     S1: DataClone<Elem = R>,
     S2: DataClone<Elem = R>,
@@ -25,3 +26,5 @@ where
     pub cov: ArrayBase<S2, Ix2>,
     pub projection: ArrayBase<S3, Ix2>,
 }
+
+pub type PGaussian = PGaussianBase<OwnedRepr<R>, OwnedRepr<R>, OwnedRepr<R>>;
