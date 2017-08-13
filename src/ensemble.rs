22db09a2bdf540803b234390e72ec183fa43609c
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-08-13 22:07:21 +0900

    Remove *Base struct for simplicty

--- src/ensemble.rs
+++ src/ensemble.rs
@@ -1,94 +1,62 @@
-
 use ndarray::*;
-use ndarray_linalg::vector::outer;
+use ndarray_linalg::*;
 use ndarray_rand::RandomExt;
 use rand::distributions::*;
-use std::ops::*;
 
 use super::gaussian::Gaussian;
 use super::types::*;
 
-/// Ensemble is saved as two-dimensional array
+/// Ensemble as two-dimensional array
 #[derive(Debug, Clone)]
-pub struct EnsembleBase<S: DataClone<Elem = R>>(ArrayBase<S, Ix2>);
-pub type Ensemble = EnsembleBase<OwnedRepr<R>>;
+pub struct Ensemble(Array<R, Ix2>);
 
-impl<S: DataClone<Elem = R>> EnsembleBase<S> {
+impl Ensemble {
     /// size of ensemble
     pub fn size(&self) -> usize {
-        self.rows()
+        self.0.rows()
     }
 
     /// size of each state vector
     pub fn dim(&self) -> usize {
-        self.cols()
+        self.0.cols()
     }
 
     /// immutable ensemble iterator
-    pub fn eiter(&self) -> iter::AxisIter<R, Ix1> {
-        self.axis_iter(Axis(0))
+    pub fn ens_iter(&self) -> iter::AxisIter<R, Ix1> {
+        self.0.axis_iter(Axis(0))
     }
 
     /// mutable ensemble iterator
-    pub fn eiter_mut(&mut self) -> iter::AxisIterMut<R, Ix1>
-    where
-        S: DataMut<Elem = R>,
-    {
-        self.axis_iter_mut(Axis(0))
+    pub fn ens_iter_mut(&mut self) -> iter::AxisIterMut<R, Ix1> {
+        self.0.axis_iter_mut(Axis(0))
     }
 
     /// center of ensemble
     pub fn center(&self) -> Array<R, Ix1> {
-        self.mean(Axis(0))
+        self.0.mean(Axis(0))
     }
 
     /// regard ensemble as a Gaussian distribution
     pub fn as_gaussian(&self) -> Gaussian {
         // XXX this may be slow.
         let c = self.center();
-        let n = self.dim();
-        let m = self.size();
-        let mut cov = Array::zeros((n, n));
-        for v in self.eiter() {
-            let dx = &v - &c;
-            cov = cov + outer(&dx, &dx);
-        }
-        cov *= 1.0 / (m as f64 - 1.0);
+        let dx = &self.0 - &c;
+        let mut cov = dx.t().dot(&dx);
+        let m = self.size() as f64;
+        cov *= 1.0 / (m - 1.0);
         Gaussian {
             center: c,
-            cov: cov,
+            precision: cov.inv().unwrap(),
         }
     }
 }
 
-impl<S: DataClone<Elem = R>> From<ArrayBase<S, Ix2>> for EnsembleBase<S> {
-    fn from(a: ArrayBase<S, Ix2>) -> Self {
-        EnsembleBase(a)
-    }
-}
-
-impl<S: DataClone<Elem = R>> Deref for EnsembleBase<S> {
-    type Target = ArrayBase<S, Ix2>;
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl<S: DataClone<Elem = R>> DerefMut for EnsembleBase<S> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
-    }
-}
-
-impl<S: DataClone<Elem = R>> EnsembleBase<S> {
+impl Ensemble {
     /// Generate ensemble as an isotropic Gaussian distribution
-    pub fn isotropic_gaussian<S1>(center: &ArrayBase<S1, Ix1>, size: usize, noise: R) -> Ensemble
-    where
-        S1: Data<Elem = R>,
-    {
+    pub fn isotropic_gaussian<S: Data<Elem = R>>(center: &ArrayBase<S, Ix1>, size: usize, noise: R) -> Ensemble {
         let n = center.len();
         let dist = Normal::new(0.0, noise);
         let dx = Array::random((size, n), dist);
-        (dx + center).into()
+        Ensemble(dx + center)
     }
 }
--- src/gaussian.rs
+++ src/gaussian.rs
@@ -1,27 +1,15 @@
-
 use super::types::*;
 use ndarray::*;
 
 #[derive(Debug, Clone)]
-pub struct GaussianBase<S1, S2>
-where
-    S1: DataClone<Elem = R>,
-    S2: DataClone<Elem = R>,
-{
-    pub center: ArrayBase<S1, Ix1>,
-    pub cov: ArrayBase<S2, Ix2>,
+pub struct Gaussian {
+    pub center: Array<R, Ix1>,
+    pub precision: Array<R, Ix2>,
 }
 
-pub type Gaussian = GaussianBase<OwnedRepr<R>, OwnedRepr<R>>;
-
 #[derive(Debug, Clone)]
-pub struct ProjectedGaussian<S1, S2, S3>
-where
-    S1: DataClone<Elem = R>,
-    S2: DataClone<Elem = R>,
-    S3: DataClone<Elem = R>,
-{
-    pub center: ArrayBase<S1, Ix1>,
-    pub cov: ArrayBase<S2, Ix2>,
-    pub projection: ArrayBase<S3, Ix2>,
+pub struct ProjectedGaussian {
+    pub center: Array<R, Ix1>,
+    pub precision: Array<R, Ix2>,
+    pub p: Array<R, Ix2>,
 }
