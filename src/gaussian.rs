137a899c479274506cd2adca71495dbadd32b4a3
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-08-16 18:11:54 +0900

    Merge two Gaussians

--- src/gaussian.rs
+++ src/gaussian.rs
@@ -1,5 +1,6 @@
 use super::types::*;
 use ndarray::*;
+use ndarray_linalg::*;
 
 #[derive(Debug, Clone)]
 pub struct Gaussian {
@@ -12,4 +13,15 @@ pub struct ProjectedGaussian {
     pub center: Array<R, Ix1>,
     pub precision: Array<R, Ix2>,
     pub p: Array<R, Ix2>,
+}
+
+pub fn merge(a: &Gaussian, b: &Gaussian) -> Gaussian {
+    let p = &a.precision + &b.precision;
+    let c = a.precision.dot(&a.center) + b.precision.dot(&b.center);
+    let f: Factorized<OwnedRepr<R>> = p.factorize().unwrap();
+    let c = f.solve(Transpose::No, c).unwrap(); // FIXME `solve` uses LU decomposition
+    Gaussian {
+        center: c,
+        precision: p,
+    }
 }
