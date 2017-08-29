0a7339dba425b87f926971acb06d5d0c2bf3bac5
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-08-29 17:33:10 +0900

    Bug fix about removing into_{e,m}

--- tests/gaussian.rs
+++ tests/gaussian.rs
@@ -10,11 +10,11 @@ use ndarray_linalg::*;
 
 #[test]
 fn merge_e() {
-    let g1 = Gaussian::from_mean(array![1.0, 0.0], array![[1.0, 0.0], [0.0, 1.0]]).into_e();
-    let g2 = Gaussian::from_mean(array![1.0, 0.0], array![[1.0, 0.0], [0.0, 1.0]]).into_e();
+    let g1: E = Gaussian::from_mean(array![1.0, 0.0], array![[1.0, 0.0], [0.0, 1.0]]).into();
+    let g2: E = Gaussian::from_mean(array![1.0, 0.0], array![[1.0, 0.0], [0.0, 1.0]]).into();
     let g3e = &g1 * &g2;
     println!("g3E = {:?}", &g3e);
-    let g3m = g3e.into_m();
+    let g3m: M = g3e.into();
     println!("g3M = {:?}", &g3m);
     assert_close_l2!(&g3m.center, &array![1.0, 0.0], 1e-7);
     assert_close_l2!(&g3m.cov, &array![[0.5, 0.0], [0.0, 0.5]], 1e-7);
