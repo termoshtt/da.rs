f622b37e93dffb4f20b9946e50cdf0741ae49191
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-12-28 14:54:35 +0900

    Add test of `Into` of Gaussian

--- tests/gaussian.rs
+++ tests/gaussian.rs
@@ -25,3 +25,25 @@ fn e2m2e() {
     assert_close_l2!(&e.prec, &e2.prec, 1e-7);
     assert_close_l2!(&e.ab, &e2.ab, 1e-7);
 }
+
+#[test]
+fn m2e2m_into() {
+    let n = 5;
+    let m = M::random(n);
+    let m0 = m.clone();
+    let e: E = m.into();
+    let m: M = e.into();
+    assert_close_l2!(&m.center, &m0.center, 1e-7);
+    assert_close_l2!(&m.cov, &m0.cov, 1e-7);
+}
+
+#[test]
+fn e2m2e_into() {
+    let n = 5;
+    let e = E::random(n);
+    let e0 = e.clone();
+    let m: M = e.into();
+    let e: E = m.into();
+    assert_close_l2!(&e.prec, &e0.prec, 1e-7);
+    assert_close_l2!(&e.ab, &e0.ab, 1e-7);
+}
