84e5bdf4fac6f2efa1428cae3cbe6fade459dd6d
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-12-28 15:21:26 +0900

    Drop Analysis/Forecast trait (postponed to future PR)

--- src/lib.rs
+++ src/lib.rs
@@ -25,15 +25,3 @@ pub struct State(Array1<R>);
 /// Obverstion
 #[derive(Debug, Clone, NewType)]
 pub struct Obs(Array1<R>);
-
-/// Trait for Analysis step in DA.
-pub trait Analysis {
-    type PDF;
-    fn analysis(&self, &mut Self::PDF, &Obs);
-}
-
-/// Trait for Forecast step in DA.
-pub trait Forecast {
-    type PDF;
-    fn forecast(&self, &mut Self::PDF);
-}
