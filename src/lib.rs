e2e7699ec2d54718d825ffe7ccdcbbb1ceab8fda
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-12-28 17:07:44 +0900

    Strong typedef for Sizes

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 
 #[macro_use]
 extern crate procedurals;
+#[macro_use]
+extern crate derive_new;
 
 extern crate num_complex;
 extern crate ndarray;
@@ -18,24 +20,46 @@ pub type R = f64;
 /// Complex number
 pub type C = Complex<f64>;
 
+pub trait Size: Copy + Eq + Ord + Into<usize> + From<usize> {}
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Ord, PartialOrd, NewType, new)]
+pub struct StateSize(usize);
+impl Size for StateSize {}
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Ord, PartialOrd, NewType, new)]
+pub struct ObsSize(usize);
+impl Size for ObsSize {}
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Ord, PartialOrd, NewType, new)]
+pub struct EnsembleSize(usize);
+impl Size for EnsembleSize {}
+
 /// State of the simulation
-#[derive(Debug, Clone, NewType)]
+#[derive(Debug, Clone, NewType, new)]
 pub struct State(Array1<R>);
 
 impl State {
     /// Generate random state
-    pub fn random(n: usize) -> Self {
-        ndarray_linalg::random(n).into()
+    pub fn random(n: StateSize) -> Self {
+        ndarray_linalg::random(n.0).into()
+    }
+
+    pub fn size(&self) -> StateSize {
+        self.len().into()
     }
 }
 
 /// Obverstion
-#[derive(Debug, Clone, NewType)]
+#[derive(Debug, Clone, NewType, new)]
 pub struct Obs(Array1<R>);
 
 impl Obs {
     /// Generate random observation
-    pub fn random(n: usize) -> Self {
-        ndarray_linalg::random(n).into()
+    pub fn random(n: ObsSize) -> Self {
+        ndarray_linalg::random(n.0).into()
+    }
+
+    pub fn size(&self) -> ObsSize {
+        self.len().into()
     }
 }
